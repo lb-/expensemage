fb6928138acb4b2458ad815c2495b29b158a5fa4
LB Johnston <mail@lb.ee>
2016-03-25 17:35:52 +1000

    get form submit working

--- client/services/submit.js
+++ client/services/submit.js
@@ -1,11 +1,43 @@
+import reqwest from 'reqwest';
+import R from 'ramda';
+
 const messageFormat = {role: 'submit'};
 
-const messageHandler = function (msg, done) {
-  //this should process the message data
+const parseData = R.curry(function(data) {
   //check data is there
   //check data format
-  //send it to a server
-  return done(null, {answer: (msg.data), serverSays: 'yay'})
+  return JSON.stringify(data);
+});
+
+const postDataReqestCors = R.curry(function (success, error, url, data) {
+  //not pure as it does not return anything
+  reqwest({url, data, success, error,
+    type: 'json',
+    method: 'post',
+    contentType: 'application/json; charset=utf-8',
+    crossOrigin: true,
+  });
+});
+
+
+const messageHandler = function (msg, done) {
+  const myjsonApi = 'https://api.myjson.com/bins';
+
+  const whenDone = R.curry(function (done, msg) {
+    done(null, msg);
+  });
+
+  const whenError = R.curry(function (done, error) {
+    done(error);
+  });
+
+  const submitRequest = R.compose(
+    postDataReqestCors(whenDone(done), whenError(done), myjsonApi),
+    parseData
+  );
+
+  //send it to server
+  submitRequest(msg.data);
 };
 
 export default function (varo) {
